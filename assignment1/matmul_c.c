ea363aaab3d9227c6ac649c9265142ad98f01b85
Markussorensen <s183997@student.dtu.dk>
2023-01-05 12:14:21 +0100

    all

--- assignment1/matmul_c.c
+++ assignment1/matmul_c.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <cblas.h>
+#include <math.h>
 
 void matmult_nat(int M, int N, int K, double **A, double **B, double **C) {
     for (int m = 0; m < M; m++) {
@@ -111,5 +112,31 @@ void matmult_knm(int M, int N, int K, double **A, double **B, double **C) {
         }
     }
 }
+
+void matmult_blk(int M,int N,int K,double **A,double **B,double **C, int bs) {
+    for (int m = 0; m < M; m++) {
+        for (int n = 0; n < N; n++) {
+            C[m][n] = 0.0;
+            }
+        }
+    for (int i = 0; i < M; i += bs) {
+         int row_min = fmin(i + bs, M);
+        for (int j = 0; j < N; j += bs) {
+            int col_min = fmin(j + bs, N);
+            for (int k = 0; k < K; k += bs) {
+                int dot_min = fmin(k + bs, K);
+                for (int ii = i; ii < row_min; ii++) {
+                    double *Aii = A[ii];
+                    for (int jj = j; jj < col_min; jj++) {
+                        for (int kk = k; kk < dot_min; kk++) {
+                            C[ii][jj] += Aii[kk] * B[kk][jj];
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 
 

6eee858582410ddd90462b9f07e754e60dc24018
Markussorensen <s183997@student.dtu.dk>
2023-01-20 19:32:17 +0100

    all

--- assignment3/02614_Assignment3_poisson_tools/matmult_test.sh
+++ assignment3/02614_Assignment3_poisson_tools/matmult_test.sh
@@ -15,18 +15,23 @@
 #BSUB -W 60
 #BSUB -R "span[hosts=1]"
 
-module load nvhpc/22.11-nompi
-module load cuda/11.8
-module load gcc/11.3.0-binutils-2.38
+nvidia-smi
 
-MAX_ITERS=5000
+sudo lshw -C display
+lscpu
 
-for method in 1 2 3 4 5; do
-    echo " "
-    echo "Testing for $method method"
-    for size in 50 100 150 200 250 300; do
-        echo "Size: $size"
-        OMP_NUM_THREADS=16
-        ./poisson $size $MAX_ITERS 0.000001 0.0 $method 
-    done
-done
+# module load nvhpc/22.11-nompi
+# module load cuda/11.8
+# module load gcc/11.3.0-binutils-2.38
+
+# MAX_ITERS=5000
+
+# for method in 1 2 3 4 5; do
+#     echo " "
+#     echo "Testing for $method method"
+#     for size in 50 100 150 200 250 300; do
+#         echo "Size: $size"
+#         OMP_NUM_THREADS=16
+#         ./poisson $size $MAX_ITERS 0.000001 0.0 $method 
+#     done
+# done

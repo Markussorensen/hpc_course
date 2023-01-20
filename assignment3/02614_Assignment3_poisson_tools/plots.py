6f1e5a9707cae609d5fa7c1c78e143fe59f7f99a
Markussorensen <s183997@student.dtu.dk>
2023-01-20 18:32:31 +0100

    all

---
+++ assignment3/02614_Assignment3_poisson_tools/plots.py
@@ -0,0 +1,245 @@
+# Loaded module: cuda/11.8
+# Loaded module: gcc/11.3.0-binutils-2.38
+ 
+# Testing for 1 method
+# Size: 50
+# N = 50, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 1 
+# Time: 0.513661 
+# GFLOPS/s: 8.517286 
+# Size: 100
+# N = 100, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 1 
+# Time: 5.043910 
+# GFLOPS/s: 6.939056 
+# Size: 150
+# N = 150, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 1 
+# Time: 24.179534 
+# GFLOPS/s: 4.885323 
+# Size: 200
+# N = 200, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 1 
+# Time: 71.307939 
+# GFLOPS/s: 3.926629 
+# Size: 250
+# N = 250, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 1 
+# Time: 146.621327 
+# GFLOPS/s: 3.729848 
+# Size: 300
+# N = 300, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 1 
+# Time: 249.407197 
+# GFLOPS/s: 3.788980 
+ 
+# Testing for 2 method
+# Size: 50
+# N = 50, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 2 
+# Time: 0.065050 
+# GFLOPS/s: 67.255780 
+# Size: 100
+# N = 100, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 2 
+# Time: 0.521730 
+# GFLOPS/s: 67.084467 
+# Size: 150
+# N = 150, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 2 
+# Time: 3.446248 
+# GFLOPS/s: 34.276362 
+# Size: 200
+# N = 200, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 2 
+# Time: 10.150429 
+# GFLOPS/s: 27.585021 
+# Size: 250
+# N = 250, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 2 
+# Time: 21.921542 
+# GFLOPS/s: 24.946934 
+# Size: 300
+# N = 300, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 2 
+# Time: 41.397109 
+# GFLOPS/s: 22.827651 
+ 
+# Testing for 3 method
+# Size: 50
+# N = 50, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 3 
+# Time: 0.089190 
+# GFLOPS/s: 49.052546 
+# Size: 100
+# N = 100, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 3 
+# Time: 0.234561 
+# GFLOPS/s: 149.214832 
+# Size: 150
+# N = 150, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 3 
+# Time: 0.671402 
+# GFLOPS/s: 175.937589 
+# Size: 200
+# N = 200, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 3 
+# Time: 1.446053 
+# GFLOPS/s: 193.630383 
+# Size: 250
+# N = 250, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 3 
+# Time: 2.717530 
+# GFLOPS/s: 201.239822 
+# Size: 300
+# N = 300, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 3 
+# Time: 4.617831 
+# GFLOPS/s: 204.641261 
+ 
+# Testing for 4 method
+# Size: 50
+# N = 50, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 4 
+# Time: 0.078065 
+# GFLOPS/s: 56.042889 
+# Size: 100
+# N = 100, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 4 
+# Time: 0.219840 
+# GFLOPS/s: 159.206547 
+# Size: 150
+# N = 150, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 4 
+# Time: 0.614703 
+# GFLOPS/s: 192.165665 
+# Size: 200
+# N = 200, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 4 
+# Time: 1.178834 
+# GFLOPS/s: 237.522680 
+# Size: 250
+# N = 250, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 4 
+# Time: 2.508623 
+# GFLOPS/s: 217.998173 
+# Size: 300
+# N = 300, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 4 
+# Time: 4.250478 
+# GFLOPS/s: 222.327642 
+ 
+# Testing for 5 method
+# Size: 50
+# N = 50, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 5 
+# Time: 1.605199 
+# GFLOPS/s: 2.725517 
+# Size: 100
+# N = 100, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 5 
+# Time: 11.230198 
+# GFLOPS/s: 3.116595 
+# Size: 150
+# N = 150, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 5 
+# Time: 38.961196 
+# GFLOPS/s: 3.031859 
+# Size: 200
+# N = 200, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 5 
+# Time: 92.729014 
+# GFLOPS/s: 3.019549 
+# Size: 250
+# N = 250, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 5 
+# Time: 183.247186 
+# GFLOPS/s: 2.984358 
+# Size: 300
+# N = 300, iter_max = 5000, tolerance = 0.000001, start_T = 0.000000, method = 5 
+# Time: 319.155859 
+# GFLOPS/s: 2.960932 
+
+# ------------------------------------------------------------
+# Sender: LSF System <lsfadmin@hpc.dtu.dk>
+# Subject: Job 15228344: <matmult_test> in cluster <dcc> Done
+
+# Job <matmult_test> was submitted from host <gbarlogin2> by user <s183997> in cluster <dcc> at Fri Jan 20 16:22:18 2023
+# Job was executed on host(s) <16*n-62-12-75>, in queue <hpcintrogpu>, as user <s183997> in cluster <dcc> at Fri Jan 20 16:24:00 2023
+# </zhome/69/1/137385> was used as the home directory.
+# </zhome/69/1/137385/Desktop/hpc_course/assignment3/02614_Assignment3_poisson_tools> was used as the working directory.
+# Started at Fri Jan 20 16:24:00 2023
+# Terminated at Fri Jan 20 16:45:06 2023
+# Results reported at Fri Jan 20 16:45:06 2023
+
+# Your job looked like:
+
+# ------------------------------------------------------------
+# # LSBATCH: User input
+# #!/bin/bash
+# # 02614 - High-Performance Computing, January 2022
+# # 
+# # batch script to run matmult on a decidated server in the hpcintro
+# # queue
+# #
+# # Author: Bernd Dammann <bd@cc.dtu.dk>
+# #
+# #BSUB -J matmult_test
+# #BSUB -o matmult_test%J.out
+# #BSUB -q hpcintrogpu
+# #BSUB -n 16
+# #BSUB -R "rusage[mem=8GB]"
+# #BSUB -gpu "num=1:mode=exclusive_process"
+# #BSUB -W 60
+# #BSUB -R "span[hosts=1]"
+
+# module load nvhpc/22.11-nompi
+# module load cuda/11.8
+# module load gcc/11.3.0-binutils-2.38
+
+# MAX_ITERS=5000
+
+# for method in 1 2 3 4 5; do
+#     echo " "
+#     echo "Testing for $method method"
+#     for size in 50 100 150 200 250 300; do
+#         echo "Size: $size"
+#         OMP_NUM_THREADS=16
+#         ./poisson $size $MAX_ITERS 0.000001 0.0 $method 
+#     done
+# done
+
+# ------------------------------------------------------------
+
+# Successfully completed.
+
+# Resource usage summary:
+
+#     CPU time :                                   2422.61 sec.
+#     Max Memory :                                 28 MB
+#     Average Memory :                             18.90 MB
+#     Total Requested Memory :                     131072.00 MB
+#     Delta Memory :                               131044.00 MB
+#     Max Swap :                                   -
+#     Max Processes :                              4
+#     Max Threads :                                22
+#     Run time :                                   1267 sec.
+#     Turnaround time :                            1368 sec.
+
+# The output (if any) is above this job summary.
+
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+
+methods = ["jacobi", "jacobi_omp", "jacobi_map", "jacobi_target", "jacobi_map_norm"]
+sizes = [50, 100, 150, 200, 250, 300]
+times = np.zeros((len(methods), len(sizes)))
+gflops = np.zeros((len(methods), len(sizes)))
+method_idx = 0
+size_idx = 0
+
+with open("assignment3/02614_Assignment3_poisson_tools/matmult_test15228344.out") as f:
+    lines = f.readlines()
+    for line in lines:
+        #remove multiple spaces
+        line = re.sub(' +', ' ', line)
+        linearr = line.split(" ")
+        if line.startswith("Testing for"):
+            method_idx = int(linearr[2]) - 1
+        if line.startswith("Size:"):
+            size_idx = sizes.index(int(linearr[1]))
+        if line.startswith("Time:"):
+            times[method_idx, size_idx] = float(linearr[1])
+        if line.startswith("GFLOPS/s:"):
+            gflops[method_idx, size_idx] = float(linearr[1])
+
+#Speedup for map and target compared to jacobi_omp
+plt.figure()
+plt.plot(sizes, times[1, :]/times[2, :], label="jacobi_map", marker="o")
+plt.plot(sizes, times[1, :]/times[3, :], label="jacobi_target", marker="o")
+plt.xlabel("Matrix size")
+plt.ylabel("Speedup")
+plt.legend()
+plt.savefig("assignment3/02614_Assignment3_poisson_tools/plots/speedup_map_target.png")
+
+#Speedup for map with and without norm
+plt.figure()
+plt.plot(sizes, times[4, :]/times[2, :], label="Speedup", marker="o")
+plt.xlabel("Matrix size")
+plt.ylabel("Speedup")
+plt.legend()
+plt.savefig("assignment3/02614_Assignment3_poisson_tools/plots/speedup_map_norm.png")
+
+
